9ea6636b6fe37ce4e8c3028acc5db840af611173
Joshua Lovins <joshualovins@Joshuas-MBP.lan>
2020-12-17 13:10:36 -0500

    Changes based on guided proj

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+  // const [date, setDate] = useState(null);
+  // useEffect(() => {
+  //   async function getDate() {
+  //     const res = await fetch('/api/date');
+  //     const newDate = await res.text();
+  //     setDate(newDate);
+  //   }
+  //   getDate();
+  // }, []);
+
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Josh's new web app</h1>
       <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
+        Hi
       </h2>
-      <p>
+
+      {/* <p>
         <a
           href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
           target="_blank"
@@ -49,7 +43,7 @@ function App() {
       </p>
       <br />
       <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <p>{date ? date : 'Loading date...'}</p> */}
     </main>
   );
 }
